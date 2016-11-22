1aff247fdb05f3119eec8cc7907b4ba8974ed876
le1tuan <lekoi9x@gmail.com>
2016-11-22 21:31:46 +0700

    create seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call("CategorySeeder");
         $this->call("TagSeeder");
         $this->call("NewsSeeder");
-        $this->call("NewsTagSeeder");
+//        $this->call("NewsTagSeeder");
         $this->call("NewsComment");
         Model::reguard();
         // $this->call(UsersTableSeeder::class);
